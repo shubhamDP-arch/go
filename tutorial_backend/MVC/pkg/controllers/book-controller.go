d986ae554dc45ffec2cf2e96fc18931a583e50c1
backend <shubhamprabhu21@gmail.com>
2025-01-11 21:50:56 +0530

    Go go go

---
+++ tutorial_backend/MVC/pkg/controllers/book-controller.go
@@ -0,0 +1,67 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+
+	"github.com/jaqen/go-bookstore/pkg/model"
+	"github.com/jaqen/go-bookstore/pkg/utils"
+)
+
+var NewBook model.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := model.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	bookDetails, _ := model.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request)  {
+	CreateBook := &model.Book{}
+	utils.ParseBody(r, CreateBook)
+	b:=CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+	
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request)  {
+	vars:= mux.Vars(r)
+	bookId :=vars["bookId"]
+	Id, err := strconv.ParseInt(bookId, 0,0)
+	if err != nil{
+		fmt.Println("error while parsing")
+
+	}
+	book := model.DeleteBook(Id)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request){
+	var updateBook = model.Book{}
+	utils.Parse(r, updateBook)
+}
