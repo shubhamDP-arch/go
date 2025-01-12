08e15d7d03be19569afbaa81c5efd0c4790c90a9
backend <shubhamprabhu21@gmail.com>
2025-01-12 11:37:52 +0530

    Go go go

--- Goober/src/commands/add.go
+++ Goober/src/commands/add.go
@@ -1,34 +1,82 @@
 package commands
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
-	"github.com/jaqen/goober/utils"
+	"github.com/jaqen/goober/src/utils"
 )
 
 func Add(files []string) {
-
 	if len(files) == 1 && files[0] == "." {
 		files = utils.GetAllfilesInDirectory(".")
 	}
+
 	indexPath := ".goobers/index"
+	stagedFiles := readIndex(indexPath)
+
 	indexFile, err := os.OpenFile(indexPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Println("Error opening index file:", err)
 		return
 	}
 	defer indexFile.Close()
+
 	for _, file := range files {
+		if utils.IsDirectory(file) {
+			continue
+		}
+
+		if _, exists := stagedFiles[file]; exists {
+			fmt.Printf("File already staged: %s\n", file)
+			continue
+		}
+
 		content, err := ioutil.ReadFile(file)
 		if err != nil {
 			fmt.Printf("Error reading file %s: %v\n", file, err)
 			continue
 		}
-		fileHash := utils.GenerateHash((string(content)))
-		indexFile.WriteString(fileHash)
+
+		fileHash := utils.GenerateHash(string(content))
+		_, err = indexFile.WriteString(fmt.Sprintf("%s %s\n", file, fileHash))
+		if err != nil {
+			fmt.Printf("Error writing to index file: %v\n", err)
+			continue
+		}
+
 		fmt.Printf("Added the file: %s\n", file)
 	}
+}
 
+func readIndex(indexPath string) map[string]string {
+	stagedFiles := make(map[string]string)
+
+	file, err := os.Open(indexPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return stagedFiles
+		}
+		fmt.Println("Error reading index file:", err)
+		return stagedFiles
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, " ", 2)
+		if len(parts) == 2 {
+			stagedFiles[parts[0]] = parts[1] // file path -> hash
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error scanning index file:", err)
+	}
+
+	return stagedFiles
 }
--- Goober/src/commands/commit.go
+++ Goober/src/commands/commit.go
@@ -1,6 +1,39 @@
 package commands
 
-func Commit(message string) {
-	return
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
 
+	"github.com/jaqen/goober/src/utils"
+)
+
+func Commit(message string) {
+	index := ".goobers/index"
+
+	if _, err := os.Stat(index); os.IsNotExist(err) {
+		fmt.Println("Files are not staged Commiting the cwd")
+	}
+
+	stagedFiles, err := ioutil.ReadFile(index)
+	if err != nil {
+		fmt.Println("Error bro")
+	}
+
+	CreateCommitObject(string(stagedFiles), message)
+}
+
+func CreateCommitObject(stagedFiles string, message string) {
+	timestamp := time.Now().Format(time.RFC3339)
+	commitHash := utils.GenerateHash(stagedFiles+ message+ timestamp)
+	commitPath := fmt.Sprintf(".goobers/objects/%s\n", commitHash)
+	file, err := os.Create(commitPath)
+	if err != nil {
+		fmt.Printf("Problem commiting")
+	}
+	defer file.Close()
+
+	file.WriteString(fmt.Sprintf("commit %s\n%s\n%s", commitHash, message, stagedFiles))
+	fmt.Printf("Committed with hash: %s\n", commitHash)
 }
